da14e22195345808be6e3343b1db5ff3adc2b221
Rahul Choudhary <rahulchoudharyrtg19@gmail.com>
2021-06-19 19:24:22 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ DEFAULT_FROM_EMAIL = "Rahul Choudhary <rahulchoudharyrtg19@gmail.com >"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "rahulchoudharyrtg19@gmail.com"
-EMAIL_HOST_PASSWORD = "rahulgreat"
+EMAIL_HOST_PASSWORD = "*******"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
